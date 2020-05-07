21d74eedb3f031469110390a10ed4bcd3fb75bc7
ronen7822 <63970571+ronen7822@users.noreply.github.com>
2020-05-07 12:32:21 +0300

    Update parse.c

--- parse.c
+++ parse.c
@@ -125,3 +125,94 @@ int getAddMthd(char* op) {
 	/* there is no specific character for DIR. if the label is not valid, error will be detected in second scan */
 	return DIR;
 }
+
+
+/* saves data guidance information to dataImage. expects pointer to string representation of data value 
+ * INPUT: a line
+ * OUTPUT: integer. -1 in case of an eror, 0 otherwise
+ */
+int getNumbers(char* line){
+
+	char *token;
+	char *endChar = NULL; 
+	int tempDC = DC;
+	int tempNum;
+
+	if(!checkComma(line))
+	{
+		printf("Error in line %d: Data guidance parameter is incorrect, aborting storage action\n", lineNumber);
+		DC = tempDC;
+		return -1;
+	}
+
+	token = strtok(line,","); /* brakes the string to tokens of possible integers */
+	while(token)
+	{	
+		tempNum = (int) strtol(token, &endChar, 10); /* 10 for decimal base */
+
+		if(*endChar != '\0') /* the token doesn't ends with a digit */
+		{
+			printf("Error in line %d: Could not parse natural number in parameter %s\n", lineNumber, token);
+			DC = tempDC;
+			return -1;
+		}
+		if(tempNum >= 8388608 || tempNum < -8388608 ) /* the posible range for 2^24 bits in every word of memory */
+		{
+			printf("Error in line %d: Natural number parameter %s is too large to fit in memory\n", lineNumber, token);
+			DC = tempDC;
+			return -1;
+		}
+		dataImage[DC] = tempNum; /* stores the number in the data image */
+		DC++;
+		token = strtok(NULL,",");
+	}
+	return 0;
+}
+	
+/* saves string guidance information to dataImage. expects pointer to string value 
+ * INPUT: a line
+ * OUTPUT: integer. -1 in case of an eror, 0 otherwise
+ */
+int getString(char* line){
+
+	int i=0;
+
+	while(str[i] != '\0')
+	{
+		dataImage[DC] = str[i]; /* stores the char in the data image  */
+		i++;
+		DC++;
+	}
+	dataImage[DC] = '\0'; 
+	DC++;
+	return 0;
+}
+
+/*check correct comma placement in string, expects pointer to string*/
+int checkComma(char *str)
+{
+	if(*str == ',') /* if the string starts with comma sign */
+	{
+		printf("Error in line %d: Comma is dispositioned\n", lineNumber);
+		return 0;
+	}
+	str++;
+	while(*str)
+	{
+		if(*str == ',')
+		{
+			if(*(str+1) == '\0') /* comma in the end of sentence. e.g: "abc,de," */
+			{
+				printf("Error in line %d: Comma is dispositioned\n", lineNumber);
+				return 0;
+			}
+			if(*(str+1) == ',') /* two or more comma signs in row. e.g: "abc, ,de" */
+			{
+				printf("Error in line %d: Multiple consecutive commas\n", lineNumber);
+				return 0;
+			}
+		}
+		str++;
+	}
+	return 1;
+}
