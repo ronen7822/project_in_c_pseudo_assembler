be628f62b494fc91ac6e475a8440c2ada4eaa24c
ronen7822 <63970571+ronen7822@users.noreply.github.com>
2020-05-08 14:50:48 +0300

    Update machineCode.c

--- machineCode.c
+++ machineCode.c
@@ -1,40 +1,5 @@
 #include "machineCode.h"
 
-typedef struct cmdHeader {
-	unsigned int opCode: 6; /* insturction opCode */
-	unsigned int srcAdress: 2 ; /*  addressing method for destation register */
-	unsigned int srcReg: 3 ; /* source register */
-	unsigned int destAdress: 2 ; /* addressing method for source register */
-	unsigned int destReg: 3 ; /* destation register */
-	unsigned int funct: 5 ; /* insturction funct, 0 if no funct */
-	unsigned int A: 1; /* abslute Linkange */
-	unsigned int R: 1; /* Relocatable Linkange */
-	unsigned int E: 1; /* external Linkange */
-} cmdHeader;
-
-
-typedef struct data {
-	int data: 21;
-	unsigned int A: 1;
-	unsigned int R: 1;
-	unsigned int E: 1;
-} data;
-
-typedef union { /* instruction line */
-	cmdHeader head;
-	data value;
-} line;
-
-typedef struct {
-	line codeLine;
-	int IC;
-	int L;
-} linePlus;
-
-line instIamge[MAX_PROGRAM_SIZE]; /* MAX_PROGRAM_SIZE is set to 256 */
-/* line dataImage[MAX_PROGRAM_SIZE]; */
-/* e.g : mov, r1, #4, address(r1), address(4) */
-
 
 /* codeInstruction - codes an instruction into the instrcution image
  * INPUT: instrcution name, first operand name, second operand name, and their addressing methods
