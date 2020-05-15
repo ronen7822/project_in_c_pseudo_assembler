b6e991db572148b4c37767b277cd45f99eee5328
ronen7822 <63970571+ronen7822@users.noreply.github.com>
2020-05-15 13:17:22 +0300

    Update secondScan.c

--- secondScan.c
+++ secondScan.c
@@ -1,60 +1,53 @@
 #include "def.h"
-#define max_operands 2
 
-int seocnd_scan( FILE * fp)
-{
-    int i ;
-    char * curr_line [MAX_LINE_LENGTH];  /* the current line */
-    int eror_flag = 0 ;
-    int current = 0; /* the word in the sentence*/
+int seocnd_scan( FILE * fp) {
 
+	char * currLine [MAX_LINE_LENGTH] ;  /* the current line */
+	char* argv[MAX_OP_NUM] ; /* holds arguments for command statements */
+ 	int  erorFlag = 0 ;
+	int lineNumber = 0;
+    int  op1Add, op2Add, guideType, argc, current = 0 ;
 
     rewind (fp); /* sets the pointer to the begining of the file*/
 
-    while ( fscanf(fp, "%s", curr_line) ) /* stages 1-7 */ 
-    {
-        if ( curr_line[0] == ';' )
-             continue; /* skips ';' because it is documention in assembly */
-        if ( is_symbol( curr_line, current ) ) /* stage 2 */
-             current ++ ; 
-        if  ( data_storage( curr_line, current ) ||  is_extern(curr_line, current) ) /* stage 3 */
-             continue ;
 
-        if ( is_entry(curr_line, current) ) /* stage 4 */
-        {
-            if (! findLabel ( is_symbol( curr_line, current) ) ) 
-            {
-                eror_flag = 1;
+    while ( fgets(currLine, MAX_LN_LEN + 1, fp) ) /* stages 1-7 */ 
+    {
+		++lineNumber;
+		argc = parseCommand(argv, currLine); /* breakes the line into tokens */
+
+        if ( currLine[0] == ';' )
+            continue; /* skips ';' because it is documention in assembly */
+
+		if ( (labelValid( argv[current] ) >= 0 ) /* stage 2 */
+			current++ ; 		
+		
+		guideType = getGuideType( argv[current] );
+        	if  ( guideType == DATA || guideType == STR || guideType == EXTERN  ) /* stage 3 */
+            		continue ;
+
+       	if ( guideType == ENTRY )  {   /* stage 4 */ 
+
+           	if ( symbolInTable ( argv[0] ) == NULL )  {
+				printf("error in %d: label %s was not found", lineNumber, argv[0] );
+               	erorFlag = 1;
                 continue ;
             }
-            make_entry ( is_symbol( curr_line, current) ); /* stage 5 */   
-            continue ;
-        }
-
-        for (i=current; i++; i< max_operands) /* stage 6- complete this stage */
-        {
-             if ( is_symbol( curr_line, i) )
-             {
-                  if (! findLabel ( is_symbol( curr_line, i) ) ) 
-            		eror_flag = 1;
-                  else
-                  {
- 			if ( is_extern(curr_line,i) )
-                             ; /* add to dedicated external file */
-
-                        build_word( findLabel ( is_symbol( curr_line, i) ) ); /* builds the word in maching code */
-                  }
-             }
-             continue;
-        }
+           		makeEntry ( argv[0] ); /* stage 5 */   
+            	continue ;
+       	}
+		
+		op1Add = getAddMthd(argv[current+1]);
+		op2Add = getAddMthd(argv[current+2]);
+       	/* stage 6 */
+       	buildBinaryCode2 (argv[current+1],argv[current+2], op1Add,op2Add ) ; 
         
-    }
-    /* stage 7 */
-    if ( eror_flag )       
-        return 0; /* indicates that eror was found*/
+    	}
 
-    build_output_files(fp); /* stage 8 */
+   	if ( erorFlag )   /* stage 7 */    
+       	return -1; /* indicates that eror was found*/
+
+    buildFiles(fp); /* stage 8 */
     return 1; /* indicates to the main that the seocnd scan terminated correctly*/
 }
-
 
